b4e5fede8aad59a454085b23c6a3730248911b92
khumillax <vuyosn.khumalo@gmail.com>
2023-03-27 23:36:24 +0000

    betty

--- _printf.c
+++ _printf.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- * _printf - implementing our own printf function to stdout
- *  @format: string to be printed
- *  
- *  Return: number of characters printed
+ * _printf - prints and input into the standard output
+ * @format: the format string
+ *
+ * Return: number of bytes printed
  */
-
 int _printf(const char *format, ...)
 {
 	int sum = 0;
@@ -16,31 +15,31 @@ int _printf(const char *format, ...)
 
 	va_start(ap, format);
 
-	if (!format || (format[0] == '%' && !format[1])) /*checking for NULL*/
-		return(-1);
+	if (!format || (format[0] == '%' && !format[1]))/* checking NULL character*/
+		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
-	for(p = (char *)format; *p; p++)
+	for (p = (char *)format; *p; p++)
 	{
 		init_params(&params, ap);
-		if (*p != '%') /* check for format specifiers*/
+		if (*p != '%')/*checking for the % specifier*/
 		{
 			sum += _putchar(*p);
 			continue;
 		}
 		start = p;
 		p++;
-		while (get_flag(p, &params)) /*flag for p char*/
+		while (get_flag(p, &params)) /* while char at p is flag char */
 		{
-			p++;
+			p++; /* next char */
 		}
-
 		p = get_width(p, &params, ap);
 		p = get_precision(p, &params, ap);
 		if (get_modifier(p, &params))
 			p++;
 		if (!get_specifier(p))
-			sum += print_from_to(start, p, params.l_modifier || params.h_modifier ? p - 1 :0);
+			sum += print_from_to(start, p,
+				params.l_modifier || params.h_modifier ? p - 1 : 0);
 		else
 			sum += get_print_func(p, ap, &params);
 	}
